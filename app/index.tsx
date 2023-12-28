972f077177c93b7abd63daefb0e9fa92c8aca26a
Thomas Lincoln <thmas5952@gmail.com>
2023-12-28 19:26:53 -0300

    Update 1

---
+++ app/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      {/* Isso aqui é a barra superior do app, tanto que nela ficam os icones e o titulo da página */}
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" handlePress={undefined} />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" handlePress={undefined} />
+          ),
+          headerTitle: "",
+        }}
+      />
+      {/* Aqui nós configuramos o Scroll */}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View 
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}
+        >
+          <Welcome 
+
+          />
+
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
